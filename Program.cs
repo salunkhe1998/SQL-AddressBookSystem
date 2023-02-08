5b6b58f9682c8a943a027fc274dd5f752a539530
Ashwini <salunkheashwini1998@gmail.com>
2023-02-08 15:15:22 +0530

    Greeting Messagegit add Program.cs!

--- Program.cs
+++ Program.cs
@@ -4,29 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
-
-            Console.WriteLine("Enter the first name of Person: ");
-            fname = Console.ReadLine();
-            Console.WriteLine("Enter the last name of Person: ");
-            lname = Console.ReadLine();
-            Console.WriteLine("Enter the address of Person: ");
-            addr = Console.ReadLine();
-            Console.WriteLine("Enter the city of Person: ");
-            city = Console.ReadLine();
-            Console.WriteLine("Enter the state of Person: ");
-            state = Console.ReadLine();
-            Console.WriteLine("Enter the zip code of Person: ");
-            zip = Console.ReadLine();
-            Console.WriteLine("Enter the phone number of Person: ");
-            phnumber = Console.ReadLine();
-            Console.WriteLine("Enter the mail of Person: ");
-            mail = Console.ReadLine();
-            Console.WriteLine("\n");
-
-
-
-
+            Console.WriteLine("***** Welcome to Address Book Program.*****");
         }
     }
 }
