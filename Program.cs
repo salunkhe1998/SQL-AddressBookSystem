e6f31fe744d5e995b3bea4395a36cd06a466e702
Ashwini <salunkheashwini1998@gmail.com>
2023-02-08 15:23:03 +0530

    Program file is updated

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace AddressBook
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
 {
-    class Program
+    public class AddressModel
     {
-        public static void Main(string[] args)
+        private string fname;
+        private string lname;
+        private string addr;
+        private string city;
+        private string state;
+        private string zip;
+        private string phnumber;
+        private string email;
+
+
+
+        public AddressModel(string fname, string lname, string addr, string city, string state, string zip, string phnumber, string email)
         {
-            Console.WriteLine("***** Welcome to Address Book Program.*****");
+            this.fname = fname;
+            this.lname = lname;
+            this.addr = addr;
+            this.city = city;
+            this.zip = zip;
+            this.phnumber = phnumber;
+            this.email = email;
         }
+
     }
 }
